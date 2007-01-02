0860f4d64e614df77dcaa1304698a6a4d5baac03
pavelgj <pavelgj@7e39af82-6523-0410-9183-3b1050b0db83>
2007-01-02 22:36:59 +0000

    restructure project

---
+++ test/com/jpavel/gwt/wysiwyg/test/client/Test.java
@@ -0,0 +1,60 @@
+package com.jpavel.gwt.wysiwyg.test.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TabPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.jpavel.gwt.wysiwyg.client.Editor;
+
+public class Test implements EntryPoint {
+	
+	private RootPanel getRootPanel() {
+		return RootPanel.get("gwtAppContainer");
+	}
+
+	public void onModuleLoad() {
+		TabPanel t = new TabPanel();
+		t.setWidth("100%");
+		
+		// tab 1
+		Editor editor1 = new Editor();
+		editor1.setWidth("100%");
+		editor1.setHeight("300px");
+		editor1.setHTML("<h1>First!</h1>");
+		
+		// tab 2
+		Editor editor2 = new Editor();
+		editor2.setWidth("100%");
+		editor2.setHeight("300px");
+		editor2.setHTML("<h1>Second!</h1>");
+
+		// tab 3
+		final Button button = new Button("Show Editor");
+		button.addClickListener(new ClickListener() {
+			public void onClick(Widget sender) {
+				DialogBox db = new DialogBox(true);
+				Editor editor3 = new Editor();
+				editor3.setWidth("660px");
+				editor3.setHeight("200px");
+				editor3.setHTML("<h1>In a Dialog Box!</h1>");
+				
+				db.setWidget(editor3);
+				db.setText("My Dialog");
+
+				db.show();
+				db.setPopupPosition(button.getAbsoluteLeft() + 20, button.getAbsoluteTop() + 20);
+			}
+		});
+
+		t.add(editor1, "First");
+		t.add(editor2, "Second");
+		t.add(button, "Third");
+		
+		t.selectTab(0);
+		
+		getRootPanel().add(t);
+	}
+}
