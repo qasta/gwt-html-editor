38d31f76aa0fb370e8fe4a2e676eef9fc980f931
pavelgj <pavelgj@7e39af82-6523-0410-9183-3b1050b0db83>
2007-01-25 16:23:56 +0000

    - renamed package to com.gc.gwt.*  - partially corrected CheckStyle complaints - http://eclipse-cs.sourceforge.net/

---
+++ src/com/gc/gwt/wysiwyg/client/Editor.java
@@ -0,0 +1,238 @@
+/*
+ * Copyright 2006 Pavel Jbanov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gc.gwt.wysiwyg.client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gc.gwt.wysiwyg.client.defaults.DefaultEditorToolbar;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.LoadListener;
+import com.google.gwt.user.client.ui.SourcesLoadEvents;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Rich Text Editor Widget.
+ *
+ * @author pavel.jbanov
+ */
+public class Editor extends Composite implements SourcesLoadEvents {
+
+  private EditorToolbar toolbar;
+
+  private EditorWYSIWYG wysiwyg;
+
+  private VerticalPanel container;
+
+  // internal status
+  private boolean initialized = false;
+
+  // listeners
+  private List loadListeners = new ArrayList();
+
+  public Editor() {
+    this(null);
+  }
+
+  /**
+   * TODO: javadocs.
+   *
+   * @param tlBar
+   */
+  public Editor(EditorToolbar tlBar) {
+    if (tlBar == null) {
+      toolbar = new DefaultEditorToolbar(this);
+    } else {
+      toolbar = tlBar;
+    }
+
+    wysiwyg = new EditorWYSIWYG(this);
+    wysiwyg.setWidth("100%");
+
+    addLoadListener(new LoadListener() {
+      public void onLoad(Widget sender) {
+        initialized = true;
+
+        if (tmpHTMLStorage != null) {
+          getEditorWYSIWYG().setHTML(tmpHTMLStorage);
+          tmpHTMLStorage = null;
+        }
+      }
+
+      public void onError(Widget sender) {
+        // TODO Implement error handling
+      }
+    });
+
+    container = new VerticalPanel();
+    container.setStyleName("Editor");
+    container.add(toolbar);
+    container.add(wysiwyg);
+    initWidget(container);
+  }
+
+  /**
+   * do not override it!
+   */
+  protected void onLoad() {
+    super.onLoad();
+
+    if (!initialized) {
+      load();
+    } else if (tmpHTMLStorage != null) {
+      getEditorWYSIWYG().setHTML(tmpHTMLStorage);
+      tmpHTMLStorage = null;
+    }
+  }
+
+  /**
+   * TODO: javadocs. 
+   */
+  private void load() {
+    wysiwyg.initFrame(wysiwyg.getFrame().getElement());
+
+    new Timer() {
+      public void run() {
+        if (!EditorUtils.isGecko()) {
+          wysiwyg.enableDesignMode();
+        }
+
+        new Timer() {
+          public void run() {
+            notifyLoadListeners();
+          }
+        }
+        .schedule(50);
+      }
+    }
+    .schedule(50);
+  }
+
+  /**
+   * Set editor width.
+   *
+   * @param width width
+   */
+  public void setWidth(String width) {
+    container.setWidth(width);
+  }
+
+  /**
+   * @return editor width
+   */
+  public String getWidth() {
+    return DOM.getStyleAttribute(container.getElement(), "width");
+  }
+
+  /**
+   * Set editor height.
+   *
+   * @param height new height
+   */
+  public void setHeight(String height) {
+    container.setHeight(height);
+    wysiwyg.setHeight("" + (EditorUtils.parseInt(height)
+        - toolbar.getOffsetHeight()) + "px");
+  }
+
+  /**
+   * Get editor height.
+   *
+   * @return editor height
+   */
+  public String getHeight() {
+    return DOM.getStyleAttribute(container.getElement(), "height");
+  }
+
+  /**
+   * @return editor toolbar
+   */
+  public EditorToolbar getEditorToolbar() {
+    return toolbar;
+  }
+
+  /**
+   * @return editor WYSIWYG widget
+   */
+  public EditorWYSIWYG getEditorWYSIWYG() {
+    return wysiwyg;
+  }
+
+  /**
+   * Attach a LoadListener.
+   *
+   * @param listener Load Listener to attach
+   */
+  public void addLoadListener(LoadListener listener) {
+    loadListeners.add(listener);
+  }
+
+  /**
+   * Remove load listener.
+   *
+   * @parame listener Load Listener to remove
+   */
+  public void removeLoadListener(LoadListener listener) {
+    loadListeners.remove(listener);
+  }
+
+  /**
+   * fire load listener event.
+   */
+  private void notifyLoadListeners() {
+    for (int i = 0; i < loadListeners.size(); i++) {
+      ((LoadListener) loadListeners.get(i)).onLoad(this);
+    }
+  }
+
+  /**
+   * @return HTML
+   */
+  public String getHTML() {
+    return getEditorWYSIWYG().getHTML();
+  }
+
+  private String tmpHTMLStorage = null;
+
+  /**
+   * set editor HTML.
+   *
+   * @param _html HTML
+   */
+  public void setHTML(String _html) {
+    if (isAttached()) {
+      getEditorWYSIWYG().setHTML(_html);
+    } else {
+      tmpHTMLStorage = _html;
+    }
+  }
+
+  /**
+   * Exec Midas command on this editor.
+   *
+   * @param command Midas command
+   * @param ui Show browser native UI
+   * @param value command value
+   */
+  public final void execCommand(String command, boolean ui, String value) {
+    EditorUtils.execCommand(this.getEditorWYSIWYG().getFrame().getElement(),
+        command, ui, value);
+  }
+}
---
+++ src/com/gc/gwt/wysiwyg/client/defaults/DefaultEditorToolbar.java
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2006 Pavel Jbanov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gc.gwt.wysiwyg.client.defaults;
+
+import com.gc.gwt.wysiwyg.client.Editor;
+import com.gc.gwt.wysiwyg.client.EditorColorPicker;
+import com.gc.gwt.wysiwyg.client.EditorColorSelectListener;
+import com.gc.gwt.wysiwyg.client.EditorToolbar;
+import com.gc.gwt.wysiwyg.client.EditorToolbarButton;
+import com.gc.gwt.wysiwyg.client.EditorToolbarSelect;
+import com.gc.gwt.wysiwyg.client.EditorUtils;
+import com.google.gwt.user.client.ui.ChangeListener;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Default editor toolbar implementation. 
+ *
+ * @author pavel.jbanov
+ */
+public class DefaultEditorToolbar extends EditorToolbar {
+
+  /* Fields */
+  private Editor editor;
+
+  /* Constructors */
+  public DefaultEditorToolbar(Editor edt) {
+    super();
+
+    this.editor = edt;
+
+    EditorToolbarButton removeFormat = new SimpleCommandButton(editor, DefaultConstants.BUTTON_DELETE, "RemoveFormat");
+
+    EditorToolbarButton undo = new SimpleCommandButton(editor, DefaultConstants.BUTTON_UNDO, "Undo");
+    EditorToolbarButton redo = new SimpleCommandButton(editor, DefaultConstants.BUTTON_REDO, "Redo");
+
+    EditorToolbarButton bold = new SimpleCommandButton(editor, DefaultConstants.BUTTON_BOLD, "Bold");
+    EditorToolbarButton italic = new SimpleCommandButton(editor, DefaultConstants.BUTTON_ITALIC, "Italic");
+    EditorToolbarButton underlined = new SimpleCommandButton(editor, DefaultConstants.BUTTON_UNDERLINE, "Underline");
+
+    EditorToolbarButton subscript = new SimpleCommandButton(editor, DefaultConstants.BUTTON_SUBSCRIPT, "Subscript");
+    EditorToolbarButton superscript = new SimpleCommandButton(editor, DefaultConstants.BUTTON_SUPERSCRIPT, "Superscript");
+
+    EditorToolbarButton justifyLeft = new SimpleCommandButton(editor, DefaultConstants.BUTTON_ALIGNLEFT, "JustifyLeft");
+    EditorToolbarButton justifyCenter = new SimpleCommandButton(editor, DefaultConstants.BUTTON_ALIGNCENTER, "JustifyCenter");
+    EditorToolbarButton justifyRight = new SimpleCommandButton(editor, DefaultConstants.BUTTON_ALIGNRIGHT, "JustifyRight");
+    EditorToolbarButton justifyJustify = new SimpleCommandButton(editor, DefaultConstants.BUTTON_ALIGNJUSTIFY, "JustifyFull");
+
+    EditorToolbarButton ol = new SimpleCommandButton(editor, DefaultConstants.BUTTON_OL, "InsertOrderedList");
+    EditorToolbarButton ul = new SimpleCommandButton(editor, DefaultConstants.BUTTON_UL, "InsertUnorderedList");
+
+    EditorToolbarButton link = new EditorToolbarButton(editor, DefaultConstants.BUTTON_LINK);
+    link.addClickListener(new ClickListener() {
+      public void onClick(Widget sender) {
+        new SimpleOneFieldPromptPanel(DefaultEditorToolbar.this.editor, "CreateLink", "Create Link", "Link URL: ", "Create Link").show(editor);
+      }
+    });
+
+    EditorToolbarButton unlink = new SimpleCommandButton(editor, DefaultConstants.BUTTON_UNLINK, "UnLink");
+    EditorToolbarButton image = new EditorToolbarButton(editor, DefaultConstants.BUTTON_IMAGE);
+    image.addClickListener(new ClickListener() {
+      public void onClick(Widget sender) {
+        new SimpleOneFieldPromptPanel(DefaultEditorToolbar.this.editor, "InsertImage", "Insert Image", "Image URL: ", "Insert Image").show(editor);
+      }
+    });
+
+    final EditorColorPicker fgPicker = new EditorColorPicker("Select Text Color");
+    fgPicker.addSelectListener(new EditorColorSelectListener() {
+      public void colorSelected(String rgb) {
+        EditorUtils.restoreSelection(editor.getEditorWYSIWYG().getFrame().getElement());
+        editor.execCommand("ForeColor", false, rgb);
+        EditorUtils.doFocus(editor.getEditorWYSIWYG().getFrame().getElement());
+      }
+    });
+
+    EditorToolbarButton foreColor = new EditorToolbarButton(editor, DefaultConstants.BUTTON_TEXTCOLOR);
+    foreColor.addClickListener(new ClickListener() {
+      public void onClick(Widget sender) {
+        EditorUtils.saveSelection(editor.getEditorWYSIWYG().getFrame().getElement());
+        fgPicker.show();
+        fgPicker.setPopupPosition(editor.getAbsoluteLeft() + 50, editor.getAbsoluteTop() + 50);
+      }
+    });
+
+    final EditorColorPicker bgPicker = new EditorColorPicker("Select Background Color");
+    bgPicker.addSelectListener(new EditorColorSelectListener() {
+      public void colorSelected(String rgb) {
+        EditorUtils.restoreSelection(editor.getEditorWYSIWYG().getFrame().getElement());
+        editor.execCommand(EditorUtils.isIE() ? "backcolor" : "hilitecolor", false, rgb);
+        EditorUtils.doFocus(editor.getEditorWYSIWYG().getFrame().getElement());
+      }
+    });
+    EditorToolbarButton bgColor = new EditorToolbarButton(editor, DefaultConstants.BUTTON_TEXTBACKGROUNDCOLOR);
+    bgColor.addClickListener(new ClickListener() {
+      public void onClick(Widget sender) {
+        EditorUtils.saveSelection(editor.getEditorWYSIWYG().getFrame().getElement());
+        bgPicker.show();
+        bgPicker.setPopupPosition(editor.getAbsoluteLeft() + 50, editor.getAbsoluteTop() + 50);
+      }
+    });
+
+    EditorToolbarSelect styles = new EditorToolbarSelect();
+    styles.addItem("Style", "");
+    String[][] formats = EditorUtils.getSupportedFormats();
+    for (int i = 0; i < formats.length; i++) {
+      styles.addItem(formats[i][0], formats[i][1]);
+    }
+
+    styles.addChangeListener(new ChangeListener() {
+      public void onChange(Widget sender) {
+        ListBox subj = ((ListBox) sender);
+        String value = subj.getValue(subj.getSelectedIndex());
+        subj.setSelectedIndex(0);
+        EditorUtils.doFocus(editor.getEditorWYSIWYG().getFrame().getElement());
+        editor.execCommand("FormatBlock", false, value);
+      }
+    });
+
+    EditorToolbarSelect fontSizes = new EditorToolbarSelect();
+    fontSizes.addItem("Size", "");
+    for (int i = 1; i < 8; i++) {
+      fontSizes.addItem("Size " + i, "" + i);
+    }
+
+    fontSizes.addChangeListener(new ChangeListener() {
+      public void onChange(Widget sender) {
+        ListBox subj = ((ListBox) sender);
+        String value = subj.getValue(subj.getSelectedIndex());
+        subj.setSelectedIndex(0);
+        editor.execCommand("FontSize", false, value);
+        EditorUtils.doFocus(editor.getEditorWYSIWYG().getFrame().getElement());
+      }
+    });
+
+
+    EditorToolbarButton source = new EditorToolbarButton(editor, DefaultConstants.BUTTON_SOURCE);
+    source.addClickListener(new ClickListener() {
+      public void onClick(Widget sender) {
+        editor.getEditorWYSIWYG().toggleView();
+      }
+    });
+    this.addEditorToolbarWidget(source);
+
+    this.addEditorToolbarWidget(EditorToolbar.getSpacer());
+
+    this.addEditorToolbarWidget(removeFormat);
+
+    this.addEditorToolbarWidget(EditorToolbar.getSpacer());
+
+    this.addEditorToolbarWidget(undo);
+    this.addEditorToolbarWidget(redo);
+
+    this.addEditorToolbarWidget(EditorToolbar.getSpacer());
+
+    this.addEditorToolbarWidget(bold);
+    this.addEditorToolbarWidget(italic);
+    this.addEditorToolbarWidget(underlined);
+
+    this.addEditorToolbarWidget(EditorToolbar.getSpacer());
+
+    this.addEditorToolbarWidget(subscript);
+    this.addEditorToolbarWidget(superscript);
+
+    this.addEditorToolbarWidget(EditorToolbar.getSpacer());
+
+    this.addEditorToolbarWidget(justifyLeft);
+    this.addEditorToolbarWidget(justifyCenter);
+    this.addEditorToolbarWidget(justifyRight);
+    this.addEditorToolbarWidget(justifyJustify);
+
+    this.addEditorToolbarWidget(EditorToolbar.getSpacer());
+
+    this.addEditorToolbarWidget(ol);
+    this.addEditorToolbarWidget(ul);
+
+    this.addEditorToolbarWidget(EditorToolbar.getSpacer());
+
+    this.addEditorToolbarWidget(link);
+    this.addEditorToolbarWidget(unlink);
+    this.addEditorToolbarWidget(image);
+
+    this.addEditorToolbarWidget(EditorToolbar.getSpacer());
+
+    this.addEditorToolbarWidget(foreColor);
+    this.addEditorToolbarWidget(bgColor);
+
+    this.addEditorToolbarWidget(EditorToolbar.getSpacer());
+
+    this.addEditorToolbarWidget(styles);
+    this.addEditorToolbarWidget(fontSizes);
+
+    EditorToolbarButton source2 = new EditorToolbarButton(editor, DefaultConstants.BUTTON_BACK_TO_FULL_TOOLBAR);
+    source2.addClickListener(new ClickListener() {
+      public void onClick(Widget sender) {
+        editor.getEditorWYSIWYG().toggleView();
+      }
+    });
+
+    this.addSourceEditorToolbarWidget(source2);
+  }
+}
