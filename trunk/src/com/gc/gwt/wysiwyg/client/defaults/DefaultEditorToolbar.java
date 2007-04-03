5accee38b20ba74845a31b382c13a1ae1fa2dbdd
pavelgj <pavelgj@7e39af82-6523-0410-9183-3b1050b0db83>
2007-04-03 19:27:35 +0000

    code style clean-up

--- trunk/src/com/gc/gwt/wysiwyg/client/defaults/DefaultEditorToolbar.java
+++ trunk/src/com/gc/gwt/wysiwyg/client/defaults/DefaultEditorToolbar.java
@@ -85,5 +85,4 @@ public class DefaultEditorToolbar extends EditorToolbar {
 
     this.addSourceToolbarWidget(toolbarWidgetsFactory.getBackToRichTextWidget());
   }
-
 }
--- trunk/src/com/gc/gwt/wysiwyg/client/impl/EditorUtilsImpl.java
+++ trunk/src/com/gc/gwt/wysiwyg/client/impl/EditorUtilsImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2006 Pavel Jbanov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.gc.gwt.wysiwyg.client.impl;
 
 import com.gc.gwt.wysiwyg.client.EditorLoadListener;
@@ -53,5 +69,4 @@ public abstract class EditorUtilsImpl {
   public abstract void doFontStyle(Element oIframe, String style);
   
   public abstract void doFontSize(Element oIframe, String size);
- 
 }
